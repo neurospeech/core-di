f3706f55a01cc5082f4445e614f6955fdba03809
Akash Kava <support@neurospeech.com>
2017-05-30 16:14:31 +0530

    Fixed global disposable registration

--- NeuroSpeech.CoreDI/DIScope.cs
+++ NeuroSpeech.CoreDI/DIScope.cs
@@ -5,9 +5,106 @@ using System.Collections.Concurrent;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace NeuroSpeech.CoreDI
 {
+
+    public class ThreadSafeList<T> : IList<T>
+    {
+        private List<T> items = new List<T>();
+
+        public T this[int index] { get {
+                lock (items) {
+                    return items[index];
+                }
+            } set {
+                lock (items) {
+                    items[index] = value;
+                }
+            }
+        }
+
+        public int Count {
+            get {
+                lock (items) {
+                    return items.Count;
+                }
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            lock (items) {
+                items.Add(item);
+            }
+        }
+
+        public void Clear()
+        {
+            lock (items) {
+                items.Clear();
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            lock (items){
+                return items.Contains(item);
+            }
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            lock (items) {
+                items.CopyTo(array, arrayIndex);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            lock (items) {
+                return items.ToList<T>().GetEnumerator();
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            lock (items) {
+                return items.IndexOf(item);
+            }
+        }
+
+        public void Insert(int index, T item)
+        {
+            lock (items) {
+                items.Insert(index, item);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            lock (items) {
+                return items.Remove(item);
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            lock (items) {
+                items.RemoveAt(index);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IList<T>)this).GetEnumerator();
+        }
+    }
+
+
 
     /// <summary>
     /// 
@@ -24,13 +121,14 @@ namespace NeuroSpeech.CoreDI
         /// </value>
         public string Name { get; set; }
 
+
         private ConcurrentDictionary<Type, object> items = new ConcurrentDictionary<Type, object>();
 
-        private List<IDisposable> disposables;
+        private ThreadSafeList<IDisposable> disposables;
 
         public DIScope Parent { get; private set; }
 
-        private List<DIScope> children = new List<DIScope>();
+        private ThreadSafeList<DIScope> children = new ThreadSafeList<DIScope>();
 
         public DIScope(DIScope parent = null)
         {
@@ -89,7 +187,7 @@ namespace NeuroSpeech.CoreDI
         internal object RegisterDisposable(object v)
         {
             if (v is IDisposable d) {
-                var dl = (disposables ?? (disposables = new List<IDisposable>()));
+                var dl = (disposables ?? (disposables = new ThreadSafeList<IDisposable>()));
                 dl.Add(d);
             }
             return v;
